18d49a0a39d7e846428fe610b6fd72f5cca6bed6
andreitira <andreitira007@gmail.com>
2016-04-14 19:04:47 +0300

    script
    Versiune finala (cu data curenta)

---
+++ script.ps1
@@ -0,0 +1,12 @@
+
+
+
+echo "Inroduce-ti calea de cautare"
+$path = Read-Host
+
+echo "Introduce-ti calea unde doriti arhivele puse"
+$archivePath = Read-Host
+
+$date = Get-date
+
+ls $path -Recurse -Filter "*.txt" | Where-Object {$_.LastWriteTime -lt $date}  | foreach($_) { Compress-Archive($_.FullName) -DestinationPath $archivePath\bk -Update ; Remove-Item $_.fullname}
